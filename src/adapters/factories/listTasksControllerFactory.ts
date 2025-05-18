3d4c69e6da940f58fea2a47a26518394278a84e5
Liliana Silva <lilianaods@outlook.com>
2025-05-18 09:41:00 -0400

    feat: add listTasksControllerFactory and integrate into task routes

---
+++ src/adapters/factories/listTasksControllerFactory.ts
@@ -0,0 +1,21 @@
+import {
+  LogErrorMongoRepository,
+  TaskMongoRepository,
+} from "../../dataSources";
+import { DbListTasks } from "../../dataSources/db/dbListTasks";
+
+import { ListTasksController } from "../controllers/task/listTasks";
+import { LogErrorControllerDecorator } from "../decorators/logErrorControllerDecorator";
+import { Controller } from "../interfaces/controller";
+
+export const listTasksControllerFactory = (): Controller => {
+  const taskMongoRepository = new TaskMongoRepository();
+  const dbListTasks = new DbListTasks(taskMongoRepository);
+  const listTasksController = new ListTasksController(dbListTasks);
+  const logMongoRepository = new LogErrorMongoRepository();
+
+  return new LogErrorControllerDecorator(
+    listTasksController,
+    logMongoRepository
+  );
+};
---
+++ src/adapters/presentations/api/routes/taskRoutes.spec.ts
@@ -0,0 +1,18 @@
+import { MongoManager } from "../../../../dataSources";
+import request from "supertest";
+import app from "../config/app";
+
+describe("TaskRoutes", () => {
+  const client = MongoManager.getInstance();
+  beforeAll(async () => {
+    await client.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await client.disconnect();
+  });
+
+  test("It should return 204 if the list is empty", async () => {
+    await request(app).get("/api/tasks").expect(204);
+  });
+});
